1be1c55261030017938b9f6c0959d6f9503b797f
Matías Martín <matim_279@hotmail.com>
2026-03-29 10:48:21 -0300

    Reestructuración del repo

---
+++ src/config.py
@@ -0,0 +1,40 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+ORTOMOSAICOS_DIR = os.path.join(DATA_DIR, 'estanzuela', 'Ortomosaicos')
+RESULTS_DIR = os.path.join(BASE_DIR, 'results')
+
+BAND_SUFFIXES = {
+    'RGB': '_rgb_orthophoto.tif',
+    'MS': '_MS_orthophoto.tif',
+    'RED': '_RED_orthophoto.tif',
+    'NIR': '_NIR_orthophoto.tif'
+}
+
+
+SEGMENTATION_PARAMS = {
+    'method': 'otsu',
+    'primary_index': 'NDVI',
+    'apply_negative_buffer': True,
+    'buffer_pixels': 2  
+}
+
+VALIDATED_INDICES = [
+    'NDVI',
+    'NDRE',
+    'GNDVI',
+    'EVI',
+    'VARI',
+    'ExG',
+    'SAVI'
+]
+
+FEATURE_EXTRACTION_STATS = [
+    'min',
+    'max',
+    'mean',
+    'std'
+]
